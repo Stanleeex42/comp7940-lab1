7022dc93348fed176410ec31b11164c8705e2e86
stan <stan@example.com>
2025-01-25 23:52:31 +0800

    second commit

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ def hello():
 
 if __name__ == '__main__':
 	hello()
+
+# git process
+# > git clone [url]
+# > git add [file]
+# > git commit -m "[name]"
+# > git push -u origin main
+# > git push (same branch, subsequent push)
